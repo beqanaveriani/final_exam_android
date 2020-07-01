4299164f4f673bd8902fe951cd04b3bd95fd83d9
Shota, Firanashvili <shota.firanashvili.1@btu.edu.ge>
2020-07-01 21:46:28 +0400

    Added login activity and some TODO's

--- app/src/main/java/ge/btu/exam/themoviedb/LoginActivity.kt
+++ app/src/main/java/ge/btu/exam/themoviedb/LoginActivity.kt
@@ -1,12 +1,83 @@
 package ge.btu.exam.themoviedb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        signUpButton.setOnClickListener {
+            // TODO [SH] add sign up activity
+            finish()
+        }
+
+        signInButton.setOnClickListener {
+            doLogin()
+        }
+    }
+
+    public override fun onStart(){
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun doLogin(){
+
+        val email = emailInput.text.toString()
+        val password = passwordInput.text.toString()
+
+        if (email.isEmpty()){
+            emailInput.error = "Please provide email"
+            emailInput.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailInput.error = "Invalid email format"
+            emailInput.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()){
+            passwordInput.error = "Please provide passowrd"
+            passwordInput.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+
+                }
+            }
+
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?){
+        if (currentUser != null){
+            // TODO [BN] Open main activity
+            finish()
+        } else {
+            Toast.makeText(baseContext, "Please sign in", Toast.LENGTH_SHORT).show()
+        }
     }
 }
