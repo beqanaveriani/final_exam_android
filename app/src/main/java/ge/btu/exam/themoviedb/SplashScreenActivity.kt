f023af0f622650d9f3323f6122926aa2a8b7b1c1
Beka Naveriani <bnaver@brokers.io>
2020-07-01 20:23:52 +0400

    Initial project and splash screen for tmdb

---
+++ app/src/main/java/ge/btu/exam/themoviedb/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package ge.btu.exam.themoviedb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        tmdb_logo.alpha = 0f;
+        tmdb_logo.animate().setDuration(3500).alpha(1f).withEndAction{
+            val mainPage = Intent(this, MainActivity::class.java)
+            startActivity(mainPage)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
